a6799704dabe5484e93d80c16f0fd527900f212b
ausosawx <ausosa@tuta.io>
2023-02-06 00:18:48 +0800

    feat:add helper

---
+++ helper/conf.py
@@ -0,0 +1,13 @@
+# If your GitHub repository URL is https://github.com/username/reponame,
+# UserName is "username" and GitHubRepoName is "reponame".
+UserName = "ausosawx"
+GitHubRepoName = "build"
+# Download path for Releases packages.
+DownloadPath = "~/arch/RepoDB/releases"
+# The path to the database of your Arch local repository and name, optional,
+# they are used by repo-add.
+# If you just want to download packages, run helper.py with `--no_update_repo`
+# argument and ignore them.
+# See https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Custom_local_repository for more details.
+ArchRepoDBPath = "~/arch/RepoDB"
+ArchRepoName = "LocalRepo"
